302bac5ae4f3818738d9ca2c66a3e32db0050137
Robert Borg <robert.borg@avensia.com>
2022-09-29 15:42:04 +0200

    wip

--- src/version.ts
+++ src/version.ts
@@ -144,7 +144,7 @@ export async function fromTag(gitTag: string, includeSha: boolean = true): Promi
   const packageName = gitTagRegex.exec(gitTag)?.groups?.name;
   const version = gitTagRegex.exec(gitTag)?.groups?.versionCore!;
 
-  const prereleaseParts = gitTag.split(/-([a-zA-Z]+)\.([0-9]+)$/);
+  const prereleaseParts = gitTag.split(/-([a-zA-Z0-9\.]+)\.([0-9]+)$/);
   const prerelease: Prerelease | undefined =
     prereleaseParts.length === 4 ? { tag: prereleaseParts[1], number: Number(prereleaseParts[2]) } : undefined;
 
