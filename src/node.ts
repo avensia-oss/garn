5db4c2a6e36371dd1f34777a1b2b7a5d236afce2
Johan Nilsson <yoohahn@gmail.com>
2024-02-14 10:25:13 +0100

    fix: yarn bin exec with yarn v2+ (#38)
    * fix: yarn bin exec with yarn v2+
    
    * v0.10.0-rc.0
    
    * fix: get tags and sha sync
    
    * fix: new version
    
    * fix: revert git file
    
    * fix: set next version

---
+++ src/node.ts
@@ -0,0 +1,13 @@
+import * as os from 'os';
+import * as childProcess from 'child_process';
+import * as path from 'path';
+import { projectPath } from './index';
+import * as exec from './exec';
+
+export async function runNode(args: string[] = [], options?: childProcess.SpawnOptions) {
+  const executable = os.platform() === 'win32' ? 'node.cmd' : 'node';
+  return await exec.spawn(path.join(projectPath, executable), args, {
+    cwd: projectPath,
+    ...(options ?? {}),
+  });
+}
